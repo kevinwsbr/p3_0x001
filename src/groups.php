34686fbe3c49ca614298c35b15b4530fa9384d3a
Kevin Washington <kevin.washingtonbr@gmail.com>
2019-01-29 00:46:02 -0300

    Updating some pages

--- src/groups.php
+++ src/groups.php
@@ -29,107 +29,137 @@ $group->removeMember();
 <html lang="pt-BR">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 
-  <title><?php echo $group->getName(); ?> | iFace</title>
+    <title><?php echo $group->getName(); ?> | iFace</title>
 </head>
 
 <body>
-  <?php require_once 'components/header.php'; ?>
-  <?php require_once 'components/sidebar-profile.php'; ?>
+<?php require_once 'components/header.php'; ?>
+<?php require_once 'components/sidebar-profile.php'; ?>
 
-  <div class="col-md-6 gedf-main">
+<div class="col-md-6 gedf-main">
 
     <div class="h4"><?php echo $group->getName(); ?></div>
     <div class="h7 text-muted"><?php echo $group->getDescription(); ?></div>
-    <form action="groups.php?id=<?php echo $group->getID(); ?>&join=true" method="POST">
-    <button type="submit" class="btn btn-outline-primary btn-sm mt-2">Ingressar</button>
-    </form>
+  <?php if (!$group->checkMembership($user->getUsername(), $members)) {
+    ?>
+      <form action="groups.php?id=<?php echo $group->getID(); ?>&join=true" method="POST">
+          <button type="submit" class="btn btn-outline-primary btn-sm mt-2">Ingressar</button>
+      </form>
+    <?php
+  } ?>
 
-    <div class="card gedf-card my-4">
-      <div class="card-header">
-        <ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
-          <li class="nav-item">
-            <a class="nav-link active" id="posts-tab" data-toggle="tab" href="#posts" role="tab" aria-controls="posts"
-              aria-selected="true">Enviar mensagem</a>
-          </li>
-        </ul>
-      </div>
-      <div class="card-body">
-        <div class="tab-content" id="myTabContent">
-          <form action="groups.php?id=<?php echo $group->getID(); ?>&message=true" method="POST">
-<div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
-            <div class="form-group">
-              <label class="sr-only" for="message">post</label>
-              <textarea name="message" class="form-control" id="message" rows="3" placeholder="Insira aqui sua mensagem..."></textarea>
-            </div>
-
+  <?php if ($group->checkMembership($user->getUsername(), $members)) {
+    ?>
+      <div class="card gedf-card my-4">
+          <div class="card-header">
+              <ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
+                  <li class="nav-item">
+                      <a class="nav-link active" id="posts-tab" data-toggle="tab" href="#posts" role="tab"
+                         aria-controls="posts"
+                         aria-selected="true">Enviar mensagem</a>
+                  </li>
+              </ul>
           </div>
-        </div>
-        <div class="btn-toolbar justify-content-between">
-          <div class="btn-group">
-            <button type="submit" class="btn btn-primary">Enviar</button>
-          </div>
-        </div>
-          </form>
-          
-      </div>
-    </div>
+          <div class="card-body">
+              <div class="tab-content" id="myTabContent">
+                  <form action="groups.php?id=<?php echo $group->getID(); ?>&message=true" method="POST">
+                      <div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
+                          <div class="form-group">
+                              <label class="sr-only" for="message">post</label>
+                              <textarea name="message" class="form-control" id="message" rows="3"
+                                        placeholder="Insira aqui sua mensagem..."></textarea>
+                          </div>
 
-    <h4>Mensagens do grupo</h4>
-    <?php foreach ($messages as $msg) {?>       
-    <div class="card gedf-card my-3">
-      <div class="card-header">
-        <div class="d-flex justify-content-between align-items-center">
-          <div class="d-flex justify-content-between align-items-center">
-            <div>
-              <div class="h5 m-0"><?php echo $msg['name']?></div>
-              <div class="h7 text-muted">@<?php echo $msg['username']?></div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-      <div class="card-body">
-        <p class="card-text">
-          <?php echo $msg['content']?>
-        </p>
-      </div>
-    </div>
-    <?php } ?>
-  </div>
-  <div class="col-md-3">
-    <div class="card gedf-card">
-      <div class="card-body">
-        <h5 class="card-title">Membros</h5>
-        <ul class="list-unstyled">
-        <?php if (($user->getID() == $group->getIDAdmin())) { ?>
-        <li class="d-inline"><a href="users.php?id=<?php echo $user->getUsername() ?>"><?php echo $user->getName() ?></a></li>
-          <?php } ?>
-          <?php foreach ($members as $item) {?>
-          <div class="d-block">
-          <li class="d-inline"><a href="users.php?id=<?php echo $item['username'] ?>"><?php echo $item['name'] ?></a></li>
-            <form class="d-inline" action="groups.php?id=<?php echo $group->getID() ?>&iduser=<?php echo $item['ID'] ?>&remove=true" method="POST">
-            <?php if (($user->getID() == $group->getIDAdmin()) && ($user->getID() != $item['ID'])) { ?>
-              <button type="submit" class="btn badge badge-danger">Remover</button>
-            <?php } ?>
+                      </div>
+              </div>
+              <div class="btn-toolbar justify-content-between">
+                  <div class="btn-group">
+                      <button type="submit" class="btn btn-primary">Enviar</button>
+                  </div>
+              </div>
               </form>
-            <?php } ?>
-          </div>
-            
-        </ul>
-      </div>
-    </div>
-  </div>
-  </div>
-  </div>
 
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/popper.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
+          </div>
+      </div>
+    <?php
+  } ?>
+    <h4>Mensagens do grupo</h4>
+  <?php foreach ($messages as $msg) { ?>
+      <div class="card gedf-card my-3">
+          <div class="card-header">
+              <div class="d-flex justify-content-between align-items-center">
+                  <div class="d-flex justify-content-between align-items-center">
+                      <div>
+                          <div class="h5 m-0"><?php echo $msg['name'] ?></div>
+                          <div class="h7 text-muted">@<?php echo $msg['username'] ?></div>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+          <div class="card-body">
+              <p class="card-text">
+                <?php echo $msg['content'] ?>
+              </p>
+          </div>
+      </div>
+  <?php } ?>
+</div>
+<div class="col-md-3">
+    <div class="card gedf-card">
+        <div class="card-body">
+            <h5 class="card-title">Membros</h5>
+            <ul class="list-unstyled">
+              <?php if (($user->getID() == $group->getIDAdmin())) { ?>
+                  <li class="d-inline">você</li>
+              <?php } ?>
+              <?php foreach ($members
+
+              as $item) { ?>
+                <div class="d-block">
+                  <?php
+
+                  if ($item['ID'] != $group->getIDAdmin()) {
+                    ?>
+                      <li class="d-inline"><a href="users.php?id=<?php echo $item['username'] ?>">
+                          <?php
+
+                          if ($item['ID'] == $user->getID()) {
+                            echo "você";
+                          } else {
+                            echo $item['name'];
+                          }
+
+                          ?></a></li>
+                      <form class="d-inline"
+                            action="groups.php?id=<?php echo $group->getID() ?>&iduser=<?php echo $item['ID'] ?>&remove=true"
+                            method="POST">
+                        <?php if (($user->getID() == $group->getIDAdmin()) && ($user->getID() != $item['ID'])) { ?>
+                            <button type="submit" class="btn badge badge-danger">Remover</button>
+                        <?php } ?>
+                      </form>
+                  <?php } ?>
+                  <?php
+                  }
+                  ?>
+
+                </div>
+
+            </ul>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- src/users.php
+++ src/users.php
@@ -13,8 +13,10 @@ $user->setData($user->getUser($_SESSION['user']['username']));
 $displayedUser->setData($user->getUser($_GET['id']));
 
 $user->sendRequest();
+
 $user->confirmFriendship($displayedUser->getID());
 $user->rejectFriendship($displayedUser->getID());
+
 $message->sendMessage($_GET['id']);
 
 ?>
@@ -44,40 +46,52 @@ $message->sendMessage($_GET['id']);
         </h5>
         <h6 class="card-subtitle mb-2 text-muted">@<?php echo $displayedUser->getUsername() ?>
         </h6>
-        <form action="users.php?id=<?php echo $displayedUser->getUsername() ?>&receiver=<?php echo $displayedUser->getID() ?>&request=true" method="POST">
-        <button type="submit" class="btn btn-outline-primary btn-sm mt-2">Adicionar amigo</button>
-        </form>
-      </div>
-    </div>
-    <div class="card gedf-card mb-4">
-      <div class="card-header">
-        <ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
-          <li class="nav-item">
-            <a class="nav-link active" id="posts-tab" data-toggle="tab" href="#posts" role="tab" aria-controls="posts"
-              aria-selected="true">Enviar mensagem</a>
-          </li>
-        </ul>
-      </div>
-      <div class="card-body">
-        <div class="tab-content" id="myTabContent">
-          <form action="users.php?id=<?php echo $displayedUser->getID(); ?>&message=true" method="POST">
-<div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
-            <div class="form-group">
-              <label class="sr-only" for="message">post</label>
-              <textarea name="message" class="form-control" id="message" rows="3" placeholder="Insira aqui sua mensagem..."></textarea>
-            </div>
+          <?php
+             if (!$user->checkFriendship($displayedUser->getUsername(), $friends) && $displayedUser->getUsername() != $user->getUsername()) {?>
+                 <form action="users.php?id=<?php echo $displayedUser->getUsername() ?>&receiver=<?php echo $displayedUser->getID() ?>&request=true" method="POST">
+                     <button type="submit" class="btn btn-outline-primary btn-sm mt-2">Adicionar amigo</button>
+                 </form>
+          <?php
+             }
+          ?>
 
-          </div>
-        </div>
-        <div class="btn-toolbar justify-content-between">
-          <div class="btn-group">
-            <button type="submit" class="btn btn-primary">Enviar</button>
-          </div>
-        </div>
-          </form>
-          
+
       </div>
     </div>
+     <?php if ($displayedUser->getUsername() != $user->getUsername()) {
+     ?>
+         <div class="card gedf-card mb-4">
+             <div class="card-header">
+                 <ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
+                     <li class="nav-item">
+                         <a class="nav-link active" id="posts-tab" data-toggle="tab" href="#posts" role="tab" aria-controls="posts"
+                            aria-selected="true">Enviar mensagem</a>
+                     </li>
+                 </ul>
+             </div>
+             <div class="card-body">
+                 <div class="tab-content" id="myTabContent">
+                     <form action="users.php?id=<?php echo $displayedUser->getID(); ?>&message=true" method="POST">
+                         <div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
+                             <div class="form-group">
+                                 <label class="sr-only" for="message">post</label>
+                                 <textarea name="message" class="form-control" id="message" rows="3" placeholder="Insira aqui sua mensagem..."></textarea>
+                             </div>
+
+                         </div>
+                 </div>
+                 <div class="btn-toolbar justify-content-between">
+                     <div class="btn-group">
+                         <button type="submit" class="btn btn-primary">Enviar</button>
+                     </div>
+                 </div>
+                 </form>
+
+             </div>
+         </div>
+     <?php
+     }?>
+
   </div>
   <div class="col-md-3">
   </div>
